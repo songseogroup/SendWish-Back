26a0f7579bced1d6c8c39ab8811e1ca4de4f4ac8
moin105 <moinlatif105@gmail.com>
2024-10-14 10:54:26 +0500

    auth issue

--- src/common/middleware/login.middleware.ts
+++ src/common/middleware/login.middleware.ts
@@ -1,53 +1,53 @@
-import {
-  Injectable,
-  NestMiddleware,
-  HttpException,
-  HttpStatus,
-} from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
-import * as jwt from 'jsonwebtoken';
-
-@Injectable()
-export class LoggerMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    try {
-      const authToken = req.headers.authorization;
-      console.log(authToken);
-      const verify = jwt.verify(authToken, process.env.SECRET_KEY);
-      if (!verify) {
-        throw new HttpException('Invalid token', HttpStatus.FORBIDDEN);
-      }
-      console.log(verify);
-      req['user'] = verify;
-      next();
-    } catch (e) {
-      console.log('ERROR IN MIDDLEWARE', e);
-      throw new HttpException(e, HttpStatus.FORBIDDEN);
-    }
-  }
-}
-
-// import { Injectable, NestMiddleware ,HttpException,HttpStatus} from '@nestjs/common';
-// import { Request, Response, NextFunction} from 'express';
+// import {
+//   Injectable,
+//   NestMiddleware,
+//   HttpException,
+//   HttpStatus,
+// } from '@nestjs/common';
+// import { Request, Response, NextFunction } from 'express';
 // import * as jwt from 'jsonwebtoken';
 
 // @Injectable()
 // export class LoggerMiddleware implements NestMiddleware {
 //   use(req: Request, res: Response, next: NextFunction) {
 //     try {
-//       let authToken = req.headers.authorization;
-//       console.log(authToken)
+//       const authToken = req.headers.authorization;
+//       console.log(authToken);
 //       const verify = jwt.verify(authToken, process.env.SECRET_KEY);
 //       if (!verify) {
 //         throw new HttpException('Invalid token', HttpStatus.FORBIDDEN);
 //       }
 //       console.log(verify);
 //       req['user'] = verify;
-
 //       next();
 //     } catch (e) {
-//       console.log("ERROR IN MIDDLEWARE",e)
+//       console.log('ERROR IN MIDDLEWARE', e);
 //       throw new HttpException(e, HttpStatus.FORBIDDEN);
 //     }
 //   }
 // }
+
+import { Injectable, NestMiddleware ,HttpException,HttpStatus} from '@nestjs/common';
+import { Request, Response, NextFunction} from 'express';
+import * as jwt from 'jsonwebtoken';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    try {
+      let authToken = req.headers.authorization;
+      console.log(authToken)
+      const verify = jwt.verify(authToken, process.env.SECRET_KEY);
+      if (!verify) {
+        throw new HttpException('Invalid token', HttpStatus.FORBIDDEN);
+      }
+      console.log(verify);
+      req['user'] = verify;
+
+      next();
+    } catch (e) {
+      console.log("ERROR IN MIDDLEWARE",e)
+      throw new HttpException(e, HttpStatus.FORBIDDEN);
+    }
+  }
+}
