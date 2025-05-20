599241f5c375ad753fa130ff416c73e0d6cd1575
moin105 <moinlatif105@gmail.com>
2025-05-20 14:24:03 +0500

    updated route verifiction page

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import * as express from 'express';
 import { json, urlencoded } from 'express';
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
+  app.use('/auth/webhook', express.raw({ type: 'application/json' }));
   // Enable CORS
   app.enableCors();
   
